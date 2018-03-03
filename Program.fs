b508858cac6ed722fe0497332d1ecce23391c4fd
Alexander Andreev <andreevlex.as@gmail.com>
2018-03-03 23:21:16 +0300

    add simple example use HtmlProvider

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open FSharp.Data
+
+type NugetStats = HtmlProvider<"https://www.nuget.org/packages/OneScript">
+let rawStats = NugetStats().Tables.``Version History``
+
+[<EntryPoint>]
+let main argv =
+    rawStats.Rows |> Seq.iter (printfn "row = %A")
+
+    0 // return an integer exit code
